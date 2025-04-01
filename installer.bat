d2dfd76c9ea841003f2b11bb869e510727b57d43
T0stbrot <51487454+T0stbrot@users.noreply.github.com>
2025-04-01 19:08:53 +0200

    Added Sccripts

---
+++ installer.bat
@@ -0,0 +1,26 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+   cls
+   goto install
+) else (
+   cls
+   echo Restart as Admin
+   pause
+   exit
+)
+
+:install
+netsh advfirewall firewall add rule name=AllowICMP protocol=ICMPv4 dir=in action=allow
+netsh advfirewall firewall add rule name=AllowICMPv6 protocol=ICMPv6 dir=in action=allow
+echo Installed Firewall Rules for ICMP
+powershell -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\Windows\System32\winpr.exe"
+echo Added Windows Defender Exclusion
+curl https://winpr.t0stbrot.net/download --output C:\Windows\System32\winpr.exe
+echo Downloaded WinPR
+sc create WinPR start=auto binpath=C:\Windows\System32\winpr.exe
+echo Installed Service
+sc start WinPR
+echo Started Service
+echo Installed!
+pause
